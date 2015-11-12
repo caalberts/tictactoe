7ad8542a87cc2b3146bcefcc9e5bd8348901da52
Albert <albertlimca@gmail.com>
2015-11-12 17:32:38 +0800

    clean up

--- game/game.js
+++ game/game.js
@@ -1,31 +1,21 @@
 // select HTML elements and store them in variables
-var body = document.querySelector('body') // body element to listen for clicks
 var update = document.querySelector('.status-text') // status bar
 var tiles = Array.from(document.querySelectorAll('.tile')) // monitor tiles for 'X' and 'O'
-var reset = document.querySelector('.reset') // reset button to reset the game
 var playAgain = document.querySelector('.play-again')
 // create variables for the tic tac toe game
-var player = true,
-    moves = 1,
-    winner = '',
-    score = {
-      x: 0,
-      y: 0
-    }
+var player = true
+var moves = 1
+var winner = ''
+var score = {
+  x: 0,
+  y: 0
+}
 
 updateScore()
 update.textContent = player ? 'X\'s turn' : 'O\'s turn'
 resetBoard()
-// Event listener on reset button
-// reset.addEventListener('click', resetBoard)
 
 playAgain.addEventListener('click', resetBoard)
-
-// function to start the game
-function startGame () {
-
-
-}
 
 function resetBoard () {
   moves = 1
@@ -40,6 +30,11 @@ function disableBoard () {
   tiles.forEach(tile => {
     tile.removeEventListener('click', tictactoe)
   })
+}
+
+function updateScore () {
+  document.querySelector('.score-x').textContent = score.x
+  document.querySelector('.score-y').textContent = score.y
 }
 
 function tictactoe (event) {
@@ -86,9 +81,4 @@ function findWinner () {
   else if (winningCombination.some(combo =>
     combo.every(tileIndex =>
       tiles[tileIndex].textContent === 'O'))) return 'O'
-}
-
-function updateScore () {
-  document.querySelector('.score-x').textContent = score.x
-  document.querySelector('.score-y').textContent = score.y
 }
