90aa81505468e545f7263235358e1967302795d9
Albert <albertlimca@gmail.com>
2015-11-12 16:46:04 +0800

    draft revamp

--- game/game.js
+++ game/game.js
@@ -1,10 +1,12 @@
 // select HTML elements and store them in variables
 var body = document.querySelector('body') // body element to listen for clicks
-var title = document.querySelector('.title') // title bar
+var update = document.querySelector('.status-text') // status bar
 var tiles = Array.from(document.querySelectorAll('.tile')) // monitor tiles for 'X' and 'O'
 var reset = document.querySelector('.reset') // reset button to reset the game
 // create variables for the tic tac toe game
 var player, moves, winner
+var score = {}
+
 resetBoard()
 
 // Event listener on reset button
@@ -15,31 +17,40 @@ function resetBoard () {
   player = true
   moves = 1
   winner = ''
-  title.textContent = player ? 'X\'s turn' : 'O\'s turn'
+  score = {
+    x: 0,
+    y: 0
+  }
+  updateScore()
+  update.textContent = player ? 'X\'s turn' : 'O\'s turn'
   tiles.forEach(tile => tile.textContent = '')
-  reset.classList.add('hidden')
   body.addEventListener('click', tictactoe)
-  body.addEventListener('touchend', tictactoe)
+  console.log('board reset');
 }
 
 function tictactoe (event) {
   var tile = event.target
-  if (tile.className !== 'tile') return
+  if (!tile.className.includes('tile')) return
   if (tile.textContent) return
   tile.textContent = player ? 'X' : 'O'
   // find winner by checking tiles for 'X' and 'O'
   winner = findWinner()
   if (winner) {
-    title.textContent = winner + ' wins!'
+    update.textContent = winner + ' wins!'
     body.removeEventListener('click', tictactoe)
-    reset.classList.remove('hidden')
+    if (winner === 'X') {
+      score.x += 1
+    } else {
+      score.y += 1
+    }
+    updateScore()
   } else if (moves === 9) {
     // it's a tie
-    title.textContent = 'It\'s a tie!'
+    update.textContent = 'It\'s a tie!'
   } else {
     moves = moves + 1
     player = !player
-    title.textContent = player ? 'X\'s turn' : 'O\'s turn'
+    update.textContent = player ? 'X\'s turn' : 'O\'s turn'
   }
 }
 
@@ -58,4 +69,9 @@ function findWinner () {
   else if (winningCombination.some(combo =>
     combo.every(tileIndex =>
       tiles[tileIndex].textContent === 'O'))) return 'O'
+}
+
+function updateScore () {
+  document.querySelector('.score-x').textContent = score.x
+  document.querySelector('.score-y').textContent = score.y
 }
